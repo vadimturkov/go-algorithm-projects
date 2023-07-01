729f45b6e7f23cd6eea242105ffaefdec58c04f7
vadimturkov <vadim.turkov@gmail.com>
2023-07-01 19:02:09 +0300

    Project-5: Third milestone - add Eratosthenes sieve implementation

---
+++ 5-public-key-cryptography/3-sieve_of_eratosthenes.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vadimturkov/go-algorithm-projects/util"
+	"time"
+)
+
+func sieveOfEratosthenes(max int) []bool {
+	sieve := make([]bool, max+1)
+
+	for i := range sieve {
+		sieve[i] = true
+	}
+
+	for i := 2; i*i <= max; i++ {
+		if sieve[i] == true {
+			for j := i * i; j <= max; j += i {
+				sieve[j] = false
+			}
+		}
+	}
+
+	return sieve
+}
+
+func printSieve(sieve []bool) {
+	fmt.Printf("%d ", 2)
+	for i := 3; i <= len(sieve)-1; i += 2 {
+		if sieve[i] == true {
+			fmt.Printf("%d ", i)
+		}
+	}
+	fmt.Println()
+}
+
+func sieveToPrimes(sieve []bool) []int {
+	primes := []int{2}
+
+	for i := 3; i < len(sieve)-1; i += 2 {
+		if sieve[i] == true {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
+
+func main() {
+	max := util.GetIntInputOrExit("Max: ")
+
+	start := time.Now()
+	sieve := sieveOfEratosthenes(max)
+	elapsed := time.Since(start)
+	fmt.Printf("Elapsed: %f seconds\n", elapsed.Seconds())
+
+	if max <= 1000 {
+		printSieve(sieve)
+
+		primes := sieveToPrimes(sieve)
+		fmt.Println(primes)
+	}
+}
